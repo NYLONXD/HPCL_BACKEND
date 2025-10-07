1783f50b11540cbfc9a6f571b9578207f1dab82a
NYLONXD <himanshuj19772005@gmail.com>
2025-10-07 20:30:58 +0530

    db.js changed

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ module.exports = async function connectDB() {
   const MONGO = process.env.MONGO_URI || 'mongodb://localhost:27017/HPCL_Vendor';
   try {
     await mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB connected');
+    console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Mongo connect error', err);
     process.exit(1);
